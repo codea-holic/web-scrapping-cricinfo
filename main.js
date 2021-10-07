5bd0395b20644c8e96d9d2bccfc5c75c18616dd6
Ganesh <krganesh1611@gmail.com>
2021-10-07 15:19:20 +0530

    Extracted title for preview using JSDOM node module for review purpose

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-resul
 const axios = require("axios").default;
 const fs = require("fs");
 const path = require("path");
-// let jsdom = require("jsdom").jsdom;
+const jsdom = require("jsdom");
 
 let responsePromise = axios.get(url);
 
 responsePromise.then(function(response){
-    console.log(response.data);
-}).then(function(error){
+
+    let html = response.data;
+    let DOM = new jsdom.JSDOM(html);
+    let document = DOM.window.document;
+    let title = document.querySelector("title").textContent;
+    console.log(title);
+
+}).catch(function(error){
+    
     console.log("Encountered with an error");
 })
 
