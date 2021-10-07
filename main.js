d52f09a5603da065d552554614b4b3546f98a214
Ganesh <krganesh1611@gmail.com>
2021-10-07 17:08:54 +0530

    All the properties are alloted (Review) properties that are venue, date, teams, result

--- main.js
+++ main.js
@@ -19,20 +19,38 @@ responsePromise.then(function(response){
     console.log("Encountered with an error");
 })
 
-let allMatchesinfo = [];
+let allMatchesInfo = [];
 
 function withdrawInfo(document){
     let totalMatchCards = document.querySelectorAll("div.match-info.match-info-FIXTURES");
 
     let matchProperties = {
-        date = "",
-        venue = "",
-        team1 = "",
-        team2 = "",
-        result = ""
     }
     setProperties(matchProperties, totalMatchCards);
-    allMatchesinfo.push(match);
 }
 
+function setProperties(property, cards){
 
+    // for(let i = 0; i < cards.length; i++){
+        let venueAndDate = cards[0].querySelector(".description").textContent;
+        property.venue = venueAndDate.split(",")[1].trim();
+        property.dt = venueAndDate.split(",")[2].trim();
+        console.log(property.venue);
+        console.log(property.dt);
+
+        let teams = cards[0].querySelectorAll(".teams p.name");
+        property.team1 = teams[0].textContent;
+        property.team2 = teams[1].textContent;
+        console.log(property.team1);
+        console.log(property.team2);
+
+        let status = cards[0].querySelector("div.status-text span");
+        property.result = status.textContent;
+        console.log(property.result);
+        // console.log(status.textContent);
+
+        allMatchesInfo.push(property);
+    // }
+
+    console.log(allMatchesInfo);
+}
