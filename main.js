59861a362515a49a124c0b3d5b0695a4fe3d178a
Ganesh <krganesh1611@gmail.com>
2021-10-07 16:17:54 +0530

    Pulled required information from html using HTML declaration of withdraw information

--- main.js
+++ main.js
@@ -12,12 +12,27 @@ responsePromise.then(function(response){
     let html = response.data;
     let DOM = new jsdom.JSDOM(html);
     let document = DOM.window.document;
-    let title = document.querySelector("title").textContent;
-    console.log(title);
+    withdrawInfo(document);
 
 }).catch(function(error){
-    
+    console.log(error);
     console.log("Encountered with an error");
 })
+
+let allMatchesinfo = [];
+
+function withdrawInfo(document){
+    let totalMatchCards = document.querySelectorAll("div.match-info.match-info-FIXTURES");
+
+    let matchProperties = {
+        date = "",
+        venue = "",
+        team1 = "",
+        team2 = "",
+        result = ""
+    }
+    setProperties(matchProperties, totalMatchCards);
+    allMatchesinfo.push(match);
+}
 
 
